ccac6822529bbcba1b230313cd33faf2e03e8209
Nils Elveros <nils.elveros@tromb.com>
2026-06-01 11:10:34 +0200

    fix: added string value to kogitoproctype

--- src/main/java/se/fk/rimfrost/fframework/regel/Kogitoproctype.java
+++ src/main/java/se/fk/rimfrost/fframework/regel/Kogitoproctype.java
@@ -2,5 +2,37 @@ package se.fk.rimfrost.framework.regel;
 
 public enum Kogitoproctype
 {
-   BPMN
+   BPMN("BPMN");
+
+   private String value;
+
+   private Kogitoproctype(String value)
+   {
+      this.value = value;
+   }
+
+   @JsonValue
+   public String getValue()
+   {
+      return this.value;
+   }
+
+   @JsonCreator
+   public static Kogitoproctype fromValue(String value)
+   {
+      for (Kogitoproctype e : values())
+      {
+         if (e.value.equals(value))
+         {
+            return e;
+         }
+      }
+
+      throw new IllegalArgumentException("Unexpected value '" + value + "'");
+   }
+
+   public String toString()
+   {
+      return String.valueOf(this.value);
+   }
 }
